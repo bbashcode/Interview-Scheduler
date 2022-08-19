4fa4ad8a65a3b39c6012a46166d646f36b28686b
bbashcode <binbash.code@gmail.com>
2022-08-19 17:22:44 +0000

    add test for the DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,16 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  const formatSpots = function() {
+    if(props.spots === 0) {
+      return "no spots remaining";
+    } else if(props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining` ;
+    }
+  }
+
   const dayClass = classNames("day-list__item ", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
@@ -14,7 +24,7 @@ export default function DayListItem(props) {
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 } 
