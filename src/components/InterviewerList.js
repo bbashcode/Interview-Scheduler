b5030d6d94945fd18a949be94f1eccd0ffd5f1cb
bbashcode <binbash.code@gmail.com>
2022-08-19 21:30:45 +0000

    create InterviewerListItem and InterviewerList components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList (){
+  return (
+    <InterviewerListItem />
+  );
+};
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem () {
+  return (
+    <h3>InterviewerListItem</h3>
+  );
+};
