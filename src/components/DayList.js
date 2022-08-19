7791586d6c133bf49eed72790fe0f660dacb2c66
bbashcode <binbash.code@gmail.com>
2022-08-19 18:14:16 +0000

    refactor DayList component to map over days array

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,21 @@
 import DayListItem from "./DayListItem";
 import React from 'react';
 
-export const DayList = () =>{
-  return (
-    <DayListItem />
-  );
+export default function DayList(props){
+
+  const dayArray = props.days.map((day) => {
+    return <DayListItem 
+    key={day.id}
+    name={day.name}
+    spots={day.spots}
+    selected={day.name === props.day}
+    setDay={props.setDay}
+    />
+  })
+
+  return(
+    <ul>
+      {dayArray}
+    </ul>
+  )
 };
