c361c9a757797d5fbbaedb52f389f1b5444b773c
bbashcode <binbash.code@gmail.com>
2022-08-24 03:58:43 +0000

    add story block and mock data for InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import "components/InterviewerList.scss";
 import InterviewerListItem from './InterviewerListItem';
 
-export default function InterviewerList (){
-  return (
-    <InterviewerListItem />
-  );
+export default function InterviewerList (props){
+  const interviewerArray = props.interviewers.map((interviewer) => {
+    return <InterviewerListItem 
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    setInteviwer={interviewer.setInteviwer}
+    />
+  })
+
+  return(
+    <section className="interviewers">
+    <h4 className="interviewers__header text--light">{interviewerArray}</h4>
+    <ul className="interviewers__list"></ul>
+    </section>
+  )
+  // return (
+  //   <section className="interviewers">
+  //   <h4 className="interviewers__header text--light">Interviewer</h4>
+  //   <ul className="interviewers__list"></ul>
+  // </section>
+  // );
 };
