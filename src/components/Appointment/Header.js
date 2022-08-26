650329f633f0d6edb03d2ee7f17952e5abc6912d
bbashcode <binbash.code@gmail.com>
2022-08-26 06:17:27 +0000

    refactor the Header component

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{props.time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
