de86333f7cecccfccb115497f3edd7a8dc914456
bbashcode <binbash.code@gmail.com>
2022-08-29 14:33:01 +0000

    assign the necessary props to the InterviewerList component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,7 +21,9 @@ const Form = (props) => {
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={(id) => setInterviewer(id)} 
         />
       </section>
       <section className="appointment__card-right">
