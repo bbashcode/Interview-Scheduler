2501dcfed700f8da4030a355a8f95a53f196f772
bbashcode <binbash.code@gmail.com>
2022-08-30 03:25:01 +0000

    import components/Appointment into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from 'components/Appointment';
 
 //Mock data for DayList component
 const days = [
