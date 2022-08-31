e563baa9973dcba2502d626edbf34e7813c97304
bbashcode <binbash.code@gmail.com>
2022-08-31 05:23:37 +0000

    add test case for back limit in the custom hook test file

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import { useState } from "react";
+
 
 export default function useVisualMode(initial) {
   
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  const transition = newMode => { setMode(newMode) };
-  const back = () => {};
+  const transition = (newMode) => { 
+    const newM = setMode(newMode);
+    history.push(newM);
+  };
+  const back = () => {
+    mode = history.find((mode) => {});
+  };
 
   return { mode, transition, back };
 };
