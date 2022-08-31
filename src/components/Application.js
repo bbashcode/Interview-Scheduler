4c0548c6e7a3ed49057ba06e4164f9f04c7f28e1
bbashcode <binbash.code@gmail.com>
2022-08-31 03:26:44 +0000

    Rremove the setDays function and the function call in the effect method

--- src/components/Application.js
+++ src/components/Application.js
@@ -59,10 +59,9 @@ export default function Application(props) {
   const dailyAppointments = [];
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect =(()=>{
-    axios.get('http://localhost:8001/api/days' ).then(({response})=> setDays(response.data));
+    axios.get('http://localhost:8001/api/days' ).then(({response})=> );
   },[]);
 
   return (
