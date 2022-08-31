e8fcf219f08c56facd8924e1a1057858723bb42f
bbashcode <binbash.code@gmail.com>
2022-08-31 05:14:01 +0000

    create a transition function within useVisualMode function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,6 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-
-  return { mode };
+  const transition = newMode => { setMode(newMode) };
+  return { mode, transition };
 }
