1137f733a2abf6450a691b424ce1b0b1e98fb45f
bbashcode <binbash.code@gmail.com>
2022-08-31 03:34:20 +0000

    import getAppointmentsForDay selector and use it to return an array of Appointtment objects

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,48 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from 'components/Appointment';
 import axios from 'axios';
-
-
-//Mock data for Appoitnment component
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
+import { getAppointmentsForDay } from 'helpers/selectors';
 
 
 export default function Application(props) {
@@ -56,8 +15,7 @@ export default function Application(props) {
     appointments: {},
   });
 
-  const dailyAppointments = [];
-
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
 
   useEffect =(()=>{
