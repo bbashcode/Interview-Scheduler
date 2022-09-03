8cd64a6a3186afc339dbedd2a217fcf02b46d236
bbashcode <binbash.code@gmail.com>
2022-09-03 05:32:12 +0000

    refactor cypress test like jquery

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,8 +5,8 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-      
-    cy.contains("[data-testid=day]", "Tuesday")
+    cy.get("li")
+      .contains("[data-testid=day]", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected");
   });
