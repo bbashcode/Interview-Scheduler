24dddf102b37fa4205c2e5c8d301d62f249c7f6d
bbashcode <binbash.code@gmail.com>
2022-09-03 19:05:26 +0000

    clean up and refactor the Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -48,26 +48,29 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          day={state.day}
-          setDay={setDay}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
         />
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            value={state.day}
+            onChange={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
-      <section className="schedule"> { schedule } </section>
+      <section className="schedule">
+        {schedule}
+        <Appointment key={'final'} time={'5pm'} />
+      </section>
     </main>
   );
 }
