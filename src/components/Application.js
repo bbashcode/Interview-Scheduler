b514a3bd38d8f5dc983435a8e45303c1d5e2476e
bbashcode <binbash.code@gmail.com>
2022-09-03 18:58:45 +0000

    fix the Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -28,6 +28,23 @@ export default function Application(props) {
     );
   });
 
+  useEffect(() => {
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers'),
+    ])
+      .then(all => {
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }));
+      })
+      .catch(err => console.log(err.message));
+  }, [setState]);
+
   return (
     <main className="layout">
       <section className="sidebar">
