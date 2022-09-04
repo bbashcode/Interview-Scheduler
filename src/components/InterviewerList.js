3e931c66496799499105dd03d13dca47640a20dc
bbashcode <binbash.code@gmail.com>
2022-09-04 17:13:45 +0000

    fix InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,7 +11,7 @@ export default function InterviewerList (props){
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
+      selected={interviewer.id === props.value}
       setInterviewer={() => {props.onChange(interviewer.id)}}
     />
   })
